1efb7e4b3dcd5ee899283374ba3f34c1fbab022b
Fernando <fer.cantora90@gmail.com>
2025-10-11 15:23:33 -0300

    portfolio in progress

---
+++ src/components/i18n/layout.ts
@@ -0,0 +1,19 @@
+/** @format */
+
+const languajeList = {
+  es: "Español",
+  en: "English",
+};
+
+const layout_content = {
+  es: {
+    "layout.title": "Portfolio Fernando Cantora - Desarrollador Frontend",
+    "layout.description":
+      "Contrata a Fernando para desarrollar tus aplicaciones móviles y web.",
+  },
+  en: {
+    "layout.title": "Portfolio Fernando Cantora - Frontend Developer",
+    "layout.description":
+      "Hire Fernando to develop your mobile and web applications.",
+  },
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
